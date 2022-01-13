6c1624624f937f9e16210189e34a873609be60a2
Arnau <>
2022-01-13 10:40:34 +0100

    Recollir fruites + animació

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,12 @@ public class PlayerMove : MonoBehaviour
     public float runSpeed = 2;
     public float jumpSpeed = 3;
 
+    public bool improvedJump = true;
+
+    public float fallMultiplier = .5f;
+    public float lowJumpMultiplier = 2f;
+
+
     Rigidbody2D rg2D;
 
     // Start is called before the first frame update
@@ -22,10 +28,21 @@ public class PlayerMove : MonoBehaviour
             rg2D.velocity = new Vector2(runSpeed, rg2D.velocity.y);
         } else if (Input.GetKey("a") || Input.GetKey("left")) {
             rg2D.velocity = new Vector2(-runSpeed, rg2D.velocity.y);
-        } else if (Input.GetKey("w") || Input.GetKey("up")) {
-            rg2D.velocity = new Vector2(rg2D.velocity.x, jumpSpeed);
         } else {
             rg2D.velocity = new Vector2(0, rg2D.velocity.y);
+        }
+
+        if (Input.GetKey("space")) {
+            rg2D.velocity = new Vector2(rg2D.velocity.x, jumpSpeed);
+        } 
+
+        if (improvedJump) {
+            if (rg2D.velocity.y < 0) {
+                rg2D.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
+            }
+            if (rg2D.velocity.y > 0 && !Input.GetKey("space")) {
+                rg2D.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
+            }
         }
     }
 }
