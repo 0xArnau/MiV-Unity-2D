cad4dbd58516913dd5ebdb39df397014d828a796
Arnau <>
2022-01-19 19:27:04 +0100

    Nivell 4 amb particules

--- Assets/Scripts/FruitCollected.cs
+++ Assets/Scripts/FruitCollected.cs
@@ -8,8 +8,11 @@ public class FruitCollected : MonoBehaviour
     {
         if (collision.CompareTag("Player")) {
             GetComponent<SpriteRenderer>().enabled = false;
-            gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            
+            for(int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            }
+
             //FindObjectOfType<FruitManager>().AllFruitsCollected();
 
             Destroy(gameObject, .5f);
