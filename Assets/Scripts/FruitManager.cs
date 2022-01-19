554a948e84aec3d1744c5d031c75e0a6bd2afe53
Arnau <>
2022-01-19 17:54:47 +0100

    UI de fruites recollides

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class FruitManager : MonoBehaviour
 {
-   
+
+    public TMP_Text totalFruits;
+    public TMP_Text fruitsCollected;
+
+    private int totalFruitsInLevel;
+
+    void Start()
+    {
+        totalFruitsInLevel = transform.childCount;
+    }
+
     private void Update()
     {
         AllFruitsCollected();
+        totalFruits.text = totalFruitsInLevel.ToString();
+        fruitsCollected.text = (totalFruitsInLevel - transform.childCount).ToString();
     }
     
     public void AllFruitsCollected() 
